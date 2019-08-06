5530e2daacdf3765e88ba9b3fc95eadc15aa0246
bmitay4 <bmitay7@gmail.com>
2019-08-06 18:29:23 +0300

    catClasses update

---
+++ Practices/04-Classes/advanceCatClass.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+class Cat{
+private:
+  int itsAge; //Making this variable private, make it untoucheble from the outside
+public:
+  int getAge();
+  void setAge(int value);
+  void meow();
+};
+//Now we'll define the functions we just set in the class, using ::
+int Cat::getAge(){
+  return itsAge;
+}
+void Cat::setAge(int value){
+  itsAge = value;
+}
+void Cat::meow(){
+  cout << "Meow!\n";
+}
+
+//In the main function we'll create a cat object, and use all the function at that object
+int main(){
+  Cat Frisky;
+  Frisky.setAge(7);
+  Frisky.meow();
+  cout << "Frisky's age is " << Frisky.getAge() << endl;
+  Frisky.meow();
+/* The following won't work, as we define itsAge as a private member
+  cout << "Frisky's age is " << Frisky.itsAge << endl;
+*/
+
+  return 0;
+}
