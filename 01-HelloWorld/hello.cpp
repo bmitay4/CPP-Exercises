3e90c6df82b56aaf0008a0ac4004a881fa4d4a0c
bmitay4 <bmitay7@gmail.com>
2019-08-06 09:21:47 +0300

    update 2 ways for compiler

--- 01-HelloWorld/hello.cpp
+++ 01-HelloWorld/hello.cpp
@@ -1,12 +1,14 @@
 //First program at c++ by bmitay4
 //Should using <iostream> and NOT <iostream.h>
 //Should using namespace std for cout, or std::cout
-
+//Compiling by g++ hello.cpp -o Hello
+//OR
+//clang++-5.0 -std=c++17 hello.cpp -o hello
 #include <iostream>
 using namespace std;
 
 int main(){
     cout << "Hello World!\n";
-    
+
   return 0;
 }
