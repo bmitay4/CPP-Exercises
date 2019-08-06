52c15b87cd22b1112c5f1a343be42990997d2933
bmitay4 <bmitay7@gmail.com>
2019-08-06 09:16:06 +0300

    update helloWorld, namespace added

--- 01-HelloWorld/hello.cpp
+++ 01-HelloWorld/hello.cpp
@@ -1,6 +1,12 @@
+//First program at c++ by bmitay4
+//Should using <iostream> and NOT <iostream.h>
+//Should using namespace std for cout, or std::cout
+
 #include <iostream>
+using namespace std;
 
 int main(){
-    std::cout << "Hello World";
+    cout << "Hello World!\n";
+    
   return 0;
 }
