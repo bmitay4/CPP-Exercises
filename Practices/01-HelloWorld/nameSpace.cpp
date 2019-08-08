a9c9a5eec2bdd5748c046a52cf48e80515e4cbf0
bmitay4 <bmitay7@gmail.com>
2019-08-08 11:18:36 +0300

    nameSpace example

---
+++ Practices/01-HelloWorld/nameSpace.cpp
@@ -0,0 +1,27 @@
+//Exmaple of using namespace and the importancy of using it correctly
+#include <iostream>
+
+namespace abc{
+	int x = 123;
+	void printx() { std::cout << x << std::endl; }
+
+	namespace xyz {
+		int w = 5;
+	}
+};
+
+namespace def{
+	int x = 456;
+	void printx() { std::cout << x << std::endl; }
+};
+
+using namespace abc;
+using namespace def;
+int main() {
+  // printx();
+  //Can't call printx(), its ambiguous!
+
+	abc::printx();
+	def::printx();
+	std::cout << abc::xyz::w << std::endl;
+}
