200ae5233564c9ac9e36dea73ccf8d7cb323e87f
bmitay4 <bmitay7@gmail.com>
2019-08-27 17:14:16 +0300

    operator + overloading

---
+++ Practices/09-Overloading/Operators/addCounterV2.cpp
@@ -0,0 +1,50 @@
+//Simple way to sum two numbers, using an operator +
+#include <iostream>
+using namespace std;
+
+class Counter{
+public:
+  Counter();
+  Counter(int);
+  Counter(const Counter &);
+  ~Counter();
+  int getValue() const;
+  void setValue(int);
+  Counter operator + (const Counter &);
+private:
+  int itsValue;
+};
+Counter::Counter(){
+  itsValue = 0;
+}
+Counter::Counter(int x){
+  itsValue = x;
+}
+Counter::Counter(const Counter & rhs){
+  itsValue = rhs.getValue();
+}
+Counter::~Counter(){
+  cout << "Destructor called" << endl;
+}
+int Counter::getValue() const{
+  return itsValue;
+}
+void Counter::setValue(int x){
+  itsValue = x;
+}
+Counter Counter::operator+(const Counter &rhs){
+  return Counter(itsValue+rhs.getValue());
+}
+int main(){
+  Counter varOne(2), varTwo(4), varThree;
+  cout << "Counter varOne value: " << varOne.getValue() << endl;
+  cout << "Counter varTwo value: " << varTwo.getValue() << endl;
+  cout << "Counter varThree value: " << varThree.getValue() << endl;
+
+  //Sum the value of varOne and varTwo and insert it into varThree
+  varThree = varOne + varTwo;
+  cout << "Counter varOne value: " << varOne.getValue() << endl;
+  cout << "Counter varTwo value: " << varTwo.getValue() << endl;
+  cout << "Counter varThree value: " << varThree.getValue() << endl;
+  return 0;
+}
